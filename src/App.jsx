02e8976f090c55c09c5bf2bf2e145e90711c393f
Juan G De Jesus Torres <jdejesus93@gmail.com>
2020-10-21 02:26:52 -0400

    Added Routing

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import Header from './components/common/Header';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
+import Home from './components/common/views/Home'
+import Login from './components/common/views/Login'
+import Register from './components/common/views/Register'
+
+function App() {
+  const [links] = useState([
+    {name: "Login", url: "/login"},
+    {name: "Register", url: "/register"}
+  ])
+
+  return (
+    <Router>
+      <div className="App">
+      <Header links={links}/>
+      <div className="App-Content">
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/login">
+            <Login/> 
+          </Route>
+          <Route path="/register">
+            <Register/> 
+          </Route>
+        </Switch>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
